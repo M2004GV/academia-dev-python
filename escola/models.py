79e673f4ac7ff0ca1182b5c288805a0d653bef7f
Mateus <garciamateus285@gmail.com>
2025-12-04 13:24:32 -0300

    feat(core): criar os modelos Aluno, Curso e Matricula

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    data_ingresso = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=100)
+    carga_horaria = models.IntegerField(default=0)
+    valor_inscricao = models.DecimalField(max_digits=10, decimal_places=2)
+    status_curso = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.nome
+    
+class Matricula(models.Model):
+    STATUS_CHOICES = [
+        ('pago', 'Pago'),
+        ('pendente', 'Pendente'),
+    ]
+
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    data_matricula = models.DateField(auto_now_add=True)
+    status_pagamento = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pendente')
+    valor_pago = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)   
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('aluno', 'curso')
+
+    def __str__(self):
+        return f"{self.aluno.nome} - {self.curso.nome}"
