694923b7d2858cf7820e2c54b087908d5764e628
Mateus <garciamateus285@gmail.com>
2025-12-05 12:13:38 -0300

    chore(admin): registrar modelos e melhorar exibição no Django Admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Aluno, Curso, Matricula
 
-# Register your models here.
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_ingresso')
+    search_fields = ('nome', 'email', 'cpf')
+    list_filter = ('data_ingresso',)
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'carga_horaria', 'valor_inscricao', 'status_curso')
+    search_fields = ('nome',)
+    list_filter = ('status_curso',)
+
+
+@admin.register(Matricula)
+class MatriculaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'data_matricula', 'status_pagamento', 'valor_pago')
+    search_fields = ('aluno__nome', 'curso__nome')
+    list_filter = ('status_pagamento', 'data_matricula')
